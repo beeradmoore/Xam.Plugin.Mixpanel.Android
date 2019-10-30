391aad106fe7444b2e4e64ab173e01d9290ba190
Brad <brad@4pi.com.au>
2019-10-31 06:16:00 +1100

    Updated Android to 5.6.5, still ongoing issue of not being able to reference dll from nuget.

--- Binding/FetchJars.sh
+++ Binding/FetchJars.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-MIXPANEL_VERSION=5.6.0
+MIXPANEL_VERSION=5.6.5
 
 wget -O Jars/mixpanel-android.aar "https://search.maven.org/remotecontent?filepath=com/mixpanel/android/mixpanel-android/${MIXPANEL_VERSION}/mixpanel-android-${MIXPANEL_VERSION}.aar"
 rm -rf JavaDocs
