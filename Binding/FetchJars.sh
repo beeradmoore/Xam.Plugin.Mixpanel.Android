02d9d3f0325993a75bfc916cf5d03ba613ab7eac
Brad Moore <brad@4pi.com.au>
2019-11-01 19:22:03 +1100

    Changed wget to be curl requests as curl is installed by default on macOS, wget isnt. (there should be a windows compatible .bat or .ps1 file created at some point).

--- Binding/FetchJars.sh
+++ Binding/FetchJars.sh
@@ -2,10 +2,10 @@
 
 MIXPANEL_VERSION=5.6.5
 
-wget -O Jars/mixpanel-android.aar "https://search.maven.org/remotecontent?filepath=com/mixpanel/android/mixpanel-android/${MIXPANEL_VERSION}/mixpanel-android-${MIXPANEL_VERSION}.aar"
+curl -o Jars/mixpanel-android.aar "https://search.maven.org/remotecontent?filepath=com/mixpanel/android/mixpanel-android/${MIXPANEL_VERSION}/mixpanel-android-${MIXPANEL_VERSION}.aar"
 rm -rf JavaDocs
 mkdir JavaDocs
-wget -O JavaDocs/mixpanel-android-javadoc.jar "https://search.maven.org/remotecontent?filepath=com/mixpanel/android/mixpanel-android/${MIXPANEL_VERSION}/mixpanel-android-${MIXPANEL_VERSION}-javadoc.jar"
+curl -org JavaDocs/mixpanel-android-javadoc.jar "https://search.maven.org/remotecontent?filepath=com/mixpanel/android/mixpanel-android/${MIXPANEL_VERSION}/mixpanel-android-${MIXPANEL_VERSION}-javadoc.jar"
 pushd JavaDocs
 jar xf mixpanel-android-javadoc.jar
 rm -f mixpanel-android-javadoc.jar
