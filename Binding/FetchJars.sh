8d212b0fde9c106082a5883cde719d5b9ac44e40
Brad Moore <brad@4pi.com.au>
2020-01-13 14:17:37 +1100

    Fixed issue being unable to fetch jars.

--- Binding/FetchJars.sh
+++ Binding/FetchJars.sh
@@ -6,9 +6,9 @@ rm -f  Jars/mixpanel-android.aar
 rm -f  Jars/mixpanel-android-javadoc.jar 
 rm -f  Jars/mixpanel-android-sources.jar 
 
-curl -o Jars/mixpanel-android.aar "https://search.maven.org/remotecontent?filepath=com/mixpanel/android/mixpanel-android/${MIXPANEL_VERSION}/mixpanel-android-${MIXPANEL_VERSION}.aar"
-curl -o Jars/mixpanel-android-javadoc.jar "https://search.maven.org/remotecontent?filepath=com/mixpanel/android/mixpanel-android/${MIXPANEL_VERSION}/mixpanel-android-${MIXPANEL_VERSION}-javadoc.jar"
-curl -o Jars/mixpanel-android-sources.jar "https://search.maven.org/remotecontent?filepath=com/mixpanel/android/mixpanel-android/${MIXPANEL_VERSION}/mixpanel-android-${MIXPANEL_VERSION}-sources.jar"
+curl -L -o Jars/mixpanel-android.aar "https://search.maven.org/remotecontent?filepath=com/mixpanel/android/mixpanel-android/${MIXPANEL_VERSION}/mixpanel-android-${MIXPANEL_VERSION}.aar"
+curl -L -o Jars/mixpanel-android-javadoc.jar "https://search.maven.org/remotecontent?filepath=com/mixpanel/android/mixpanel-android/${MIXPANEL_VERSION}/mixpanel-android-${MIXPANEL_VERSION}-javadoc.jar"
+curl -L -o Jars/mixpanel-android-sources.jar "https://search.maven.org/remotecontent?filepath=com/mixpanel/android/mixpanel-android/${MIXPANEL_VERSION}/mixpanel-android-${MIXPANEL_VERSION}-sources.jar"
 
 rm -rf JavaDocs
 mkdir JavaDocs
