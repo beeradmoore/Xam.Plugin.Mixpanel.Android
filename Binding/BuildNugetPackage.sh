053152fb55fa7db90771409d221e877e638f0be9
Brad Moore <brad@4pi.com.au>
2020-09-26 16:29:11 +1000

    Updated build script to build snupkg

--- Binding/BuildNugetPackage.sh
+++ Binding/BuildNugetPackage.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ./FetchJars.sh
 msbuild Xam.Plugin.Mixpanel.Android.csproj -property:Configuration=Release -target:Clean,Build
-nuget pack Xam.Plugin.Mixpanel.Android.csproj -Properties Configuration=Release
+nuget pack Xam.Plugin.Mixpanel.Android.nuspec -Symbols -SymbolPackageFormat snupkg
